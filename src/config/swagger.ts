c47cc48837eac2d049359a562962f790d842d879
Mitch <ndianguimichelle@gmail.com>
2025-01-13 21:53:28 +0300

    fix: absolute paths again

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -36,7 +36,7 @@ const swaggerDefinition = {
 
 const options = {
   swaggerDefinition,
-  apis: ["src/routes/*.router.ts", "dist/routes/*.router.ts"],
+  apis: ["./src/routes/*.router.ts", "./dist/routes/*.router.ts"],
 }
 
 const swaggerSpec = swaggerJSDoc(options);
