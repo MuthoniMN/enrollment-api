a9ac144d5a9455f34284dabf9152fc9cb501859d
Mitch <ndianguimichelle@gmail.com>
2025-01-13 19:01:41 +0300

    feat: adding cors config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import router from "./routes/";
+import cors from "cors";
 
 const app = express();
 
@@ -9,6 +10,7 @@ config({ path: '.env.local' })
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 app.use('/api/v1/', router);
 
 const port = process.env.PORT || 5000;
